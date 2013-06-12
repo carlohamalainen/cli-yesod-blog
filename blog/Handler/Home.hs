f56cbcb08765453ae1b8cf7d0354e4b73cdc41a3
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-06-12 13:31:46 +1000

    Handler, EntryLongR route.

--- blog/Handler/Home.hs
+++ blog/Handler/Home.hs
@@ -2,38 +2,42 @@
 module Handler.Home where
 
 import Import
+import Data.Time (UTCTime, getCurrentTime, utctDay, toGregorian)
+import Model
+import Control.Monad (when)
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
+import Database.Persist.Store (PersistValue(PersistInt64))
+
+import qualified Data.Text as DT
+
 getHomeR :: Handler RepHtml
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
-    defaultLayout $ do
-        aDomId <- lift newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-postHomeR :: Handler RepHtml
-postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
+    entries <- runDB $ selectList [EntryVisible ==. True] [ Desc EntryPostedYear
+                                                          , Desc EntryPostedMonth
+                                                          , Desc EntryPostedDay
+                                                          ]
 
     defaultLayout $ do
-        aDomId <- lift newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+        setTitleI MsgWelcomeHomepage
+        [whamlet|
+$if null entries
+    <p>_{MsgNoEntries}
+$else
+    <ul>
+        $forall Entity eid (Entry title mashedTitle year month day content visible) <- entries
+            <li> <a href=@{EntryLongR year month day mashedTitle}>#{year}-#{month}-#{day} #{title}</a>
+<p>Boo</p>
+|]
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderDivs $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
+getEntryR :: EntryId -> Handler RepPlain
+getEntryR eid = do
+    entry <- runDB $ get eid
+
+    return $ RepPlain $ toContent $ show entry
+
+getEntryLongR :: Int -> Int -> Int -> Text -> Handler RepHtml
+getEntryLongR year month day mashedTitle = do
+    e <- runDB $ getBy $ EntryYMDMashed year month day mashedTitle
+
+    case e of (Just (Entity eid (Entry title' mashedTitle' year' month' day' content' visible'))) -> return $ RepHtml $ toContent content'
+              _                                                                            -> notFound
