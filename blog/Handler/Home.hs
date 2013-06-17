237e2c965e871bcda1be1934eee5bdfa61a190fb
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-06-17 00:54:42 -0400

    A blog post is Html, not Text!

--- blog/Handler/Home.hs
+++ blog/Handler/Home.hs
@@ -33,11 +33,28 @@ getEntryR :: EntryId -> Handler RepPlain
 getEntryR eid = do
     entry <- runDB $ get eid
 
-    return $ RepPlain $ toContent $ show entry
+    -- return $ RepPlain $ toContent $ show entry
+    return $ RepPlain "FIXME"
+
+-- foo (Entity cid (Comment entryId commentDate commentAuthor commentText visible))) = c
+-- FIXME This sort of pattern must be comment, so what's the right way to do it?
+-- Something like fromEntity :: Entity k e -> (k, e) ?
+foo :: Entity Comment -> (Key Comment, Comment)
+foo (Entity cid c@(Comment _ _ _ _ _)) = (cid, c)
 
 getEntryLongR :: Int -> Int -> Int -> Text -> Handler RepHtml
 getEntryLongR year month day mashedTitle = do
     e <- runDB $ getBy $ EntryYMDMashed year month day mashedTitle
 
-    case e of (Just (Entity eid (Entry title' mashedTitle' year' month' day' content' visible'))) -> return $ RepHtml $ toContent content'
+    case e of (Just (Entity eid (Entry title' mashedTitle' year' month' day' content' visible'))) -> do comments <- runDB $ selectList [CommentEntry ==. eid] []
+                                                                                                        let blerps = map (show . foo) comments
+                                                                                                        -- return $ RepHtml $ toContent (show blerps) -- content'
+                                                                                                        -- return $ RepHtml $ toContent content'
+                                                                                                        defaultLayout $ do
+                                                                                                            setTitleI MsgWelcomeHomepage
+                                                                                                            [whamlet|
+<h1>#{title'}
+<article>#{content'}
+|]
               _                                                                            -> notFound
+
