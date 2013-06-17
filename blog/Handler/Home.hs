7b9deb926e43edcb832e0f40560544a7f12a54d7
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-06-17 01:53:20 -0400

    whamlet for comments

--- blog/Handler/Home.hs
+++ blog/Handler/Home.hs
@@ -36,25 +36,27 @@ getEntryR eid = do
     -- return $ RepPlain $ toContent $ show entry
     return $ RepPlain "FIXME"
 
--- foo (Entity cid (Comment entryId commentDate commentAuthor commentText visible))) = c
--- FIXME This sort of pattern must be comment, so what's the right way to do it?
--- Something like fromEntity :: Entity k e -> (k, e) ?
-foo :: Entity Comment -> (Key Comment, Comment)
-foo (Entity cid c@(Comment _ _ _ _ _)) = (cid, c)
-
 getEntryLongR :: Int -> Int -> Int -> Text -> Handler RepHtml
 getEntryLongR year month day mashedTitle = do
     e <- runDB $ getBy $ EntryYMDMashed year month day mashedTitle
 
-    case e of (Just (Entity eid (Entry title' mashedTitle' year' month' day' content' visible'))) -> do comments <- runDB $ selectList [CommentEntry ==. eid] []
-                                                                                                        let blerps = map (show . foo) comments
-                                                                                                        -- return $ RepHtml $ toContent (show blerps) -- content'
-                                                                                                        -- return $ RepHtml $ toContent content'
+    case e of (Just (Entity eid (Entry title' mashedTitle' year' month' day' content' visible'))) -> do comments <- runDB $ selectList [CommentEntry ==. eid] [Asc CommentPosted]
                                                                                                         defaultLayout $ do
                                                                                                             setTitleI MsgWelcomeHomepage
                                                                                                             [whamlet|
 <h1>#{title'}
 <article>#{content'}
+    <section .comments>
+        <h2>_{MsgCommentsHeading}
+        <br>
+        $if null comments
+            <p>_{MsgNoComments}
+        $else
+            $forall Comment _entry posted name text visible <- map entityVal comments
+                <hr>
+                <h3>#{name}
+                <h4>#{show posted}
+                <p>#{toHtml text}
+            <hr>
 |]
               _                                                                            -> notFound
-
