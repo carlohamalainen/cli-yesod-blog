01acf5322ed732821434f46847662f2768c129ae
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-12-23 15:42:47 -0500

    Shell script for building the blog using a cabal sandbox.

---
+++ blog/build_in_sandbox.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PS4='($LINENO)+ '
+set -x
+
+rm -fr .cabal-sandbox cabal.sandbox.config dist
+
+cabal sandbox init
+
+export PATH=`pwd`/.cabal-sandbox/bin:$PATH
+
+cabal install happy alex --haddock-hyperlink-source --dependencies-only 
+cabal install happy alex --haddock-hyperlink-source
+
+cabal install --haddock-hyperlink-source --dependencies-only # Is this necessary? Why not just cabal install?
+cabal install
+
+cabal install angel-0.4.4 --haddock-hyperlink-source --dependencies-only 
+cabal install angel-0.4.4 --haddock-hyperlink-source
+
