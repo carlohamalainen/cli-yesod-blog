8cdf5e5c571d22e906cb0173eee3d5f7c4cf0595
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-05-08 20:01:09 +0800

    update to yesod-1.4

--- blog/Model.hs
+++ blog/Model.hs
@@ -1,22 +1,11 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
-
 module Model where
 
-import Prelude
-import Yesod
-import Data.Text (Text)
+import ClassyPrelude.Yesod
 import Database.Persist.Quasi
-import Data.Time (UTCTime)
-import Text.Blaze
-import Text.Blaze.Renderer.Utf8
-import Data.Maybe
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
-share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
+share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
-
-instance Show Entry where
-    show (Entry title mashedTitle year month day content visible) = "title: " ++ (show title) ++ " " ++ "mashed title: " ++ (show mashedTitle) ++ " " ++ "year: " ++ (show year) ++ " " ++ "month: " ++ (show month) ++ " " ++ "day: " ++ (show day) ++ " " ++ "content: " ++ (show $ renderHtml content) ++ " " ++ "visible: " ++ (show visible)
